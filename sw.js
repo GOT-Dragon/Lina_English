a4917295f2afded05accdcf4384581e0e4fb735d
GOT-Dragon <rim9kat@gmail.com>
2026-05-15 21:00:07 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'english-app-v99999999999999-clear-fixed';
+const CACHE_NAME = 'english-app-v999999999999999-clear-supabase';
 const ASSETS = [
   './index.html',
   './manifest.json'
